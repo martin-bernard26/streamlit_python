177e340ce29f69ed51f78a584af24d523fba748c
martin-bernard26 <119161281+martin-bernard26@users.noreply.github.com>
2024-10-29 07:37:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 masukan = st.text_input("Masukan data")
 if masukan!="":
   kumpulkan = [int(i) for i in masukan.split(",")]
+  panjang = len(kumpulkan)
   data = pd.DataFrame({'Nilai':kumpulkan})
   st.table(data)
+  plt.scatter(np.arange(panjang),np.array(kumpulkan))
   st.write(data.describe())
